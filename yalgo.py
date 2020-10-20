2c8ef7cd8a08232a7b6def90740e77d1dbd4c594
Dave Clayton <dave.clayton@leaseplan.com>
2020-10-20 19:19:05 +0200

    initial

---
+++ yalgo.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import numpy as np
+import json
+import sys
+import requests
+import matplotlib.pyplot as plt
+#%matplotlib inline
+from pyti.exponential_moving_average import exponential_moving_average as ema
+#from pyti.relative_strength_index import relative_strength_index as rsi
+
+import talib
+
+pd.set_option("display.max_rows", 150, "display.min_rows", 150)
+
+#pd.options.display.max_rows = 70
+
+print("Loading last week's trading data (1m period) for TSLA from Yahoo...")
+
+url = 'https://query1.finance.yahoo.com/v8/finance/chart/TSLA?region=US&lang=en-US&includePrePost=false&interval=1m&range=7d&corsDomain=finance.yahoo.com&.tsrc=finance'
+
+response = requests.get(url,
+                             headers = {
+                                 'User-Agent': 'request',
+                                 'Accept': 'application/json'
+                             })
+closes = response.json().get('chart').get('result')[0].get('indicators').get('quote')[0].get('close')
+times = response.json().get('chart').get('result')[0].get('timestamp')
+#print(closes)
+
+df = pd.DataFrame({'closes': closes, 'times': times})
+df['times'] = pd.to_datetime(df['times'], unit='s')
+df = df.set_index('times')
+
+# is_NaN = df.isnull()
+# row_has_NaN = is_NaN.any(axis=1)
+# rows_with_NaN = df[row_has_NaN]
+
+# print(rows_with_NaN)
+
+df = df.interpolate()
+
+# trade when:
+# a. EMA's cross
+# b. RSI >70 = overbought: do not buy
+#    RSI <30 = oversold: sell
+
+def trade(ema_fast, ema_slow, rsi, rsi_max, num_shares):
+    df['ema_fast'] = ema(df['closes'], ema_fast)
+    df['ema_slow'] = ema(df['closes'], ema_slow)
+    df['rsi'] = talib.RSI(df['closes'], rsi) # try 2 to 6; 14 is normal
+    #print(df)
+
+    df['position'] = np.where(df['ema_fast'] > df['ema_slow'], 1, 0)
+    df['signal'] = df['position'].diff()
+
+    returns = 0
+    boughtAt = 0
+    profit = 0
+    numTrades = 0
+    commission = 0.35
+    CountPL = False
+    initialInvestment = 0
+
+    for i, row in df.iterrows():
+        if row['signal'] == 1.0 and row['rsi'] < rsi_max:
+            boughtAt = row['closes']
+            initialInvestment = num_shares * boughtAt
+        elif row['signal'] == -1.0 and boughtAt > 0:
+            soldAt = row['closes']
+            priceDiff = num_shares * (soldAt - boughtAt)
+            print("Bought: "+str(boughtAt)+"  Sold:  "+str(soldAt)+"  Diff: "+str(priceDiff))
+            numTrades += 1
+            if not np.isnan(priceDiff):
+                profit += (priceDiff - 2*commission)
+    print("initialInvestment: "+str(initialInvestment))
+    print("ema_fast: "+str(ema_fast)+" ema_slow: "+str(ema_slow)+" rsi: "+str(rsi)+" trades: "+str(numTrades)+" profit: "+str(profit))
+
+# trade(5, 8)
+# trade(5, 12)
+# trade(5, 20)
+
+# trade(12, 12)
+# trade(12, 20)
+trade(12, 50, 2, 65, 100)
+
+# ema_fast: 12 ema_slow: 50 rsi: 12 trades: 39 profit: 1867.0598388671897
+# ema_fast: 12 ema_slow: 50 rsi: 12 trades: 38 profit: 2735.4701416015646
+# ema_fast: 12 ema_slow: 50 rsi: 8 trades: 38 profit: 3318.2612792968707
+# ema_fast: 12 ema_slow: 50 rsi: 6 trades: 37 profit: 2215.126171874995
+# ema_fast: 12 ema_slow: 50 rsi: 2 trades: 37 profit: 7608.0857666015545
+# ema_fast: 12 ema_slow: 50 rsi: 2 trades: 37 profit: 8048.185864257803
+# ema_fast: 12 ema_slow: 50 rsi: 2 rsi_max: 65 trades: 37 profit: 10420.295849609367
+
+#print(df)
---
+++ ydl.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import json
+import requests
+import time
+import calendar
+
+pd.set_option("display.max_rows", 150, "display.min_rows", 150)
+
+print("Loading trading data (7d @ 1m) for TSLA from Yahoo...")
+
+def download_period(t1, t2):
+  t2 = t2-1
+  url = 'https://query1.finance.yahoo.com/v8/finance/chart/TSLA?region=US&lang=en-US&includePrePost=false&interval=1m&corsDomain=finance.yahoo.com&.tsrc=finance'
+  if t1 != None and t2 != None:
+    url = url + '&period1='+str(t1)+'&period2='+str(t2)
+
+  print(url)
+
+  response = requests.get(url,
+                             headers = {
+                                 'User-Agent': 'request',
+                                 'Accept': 'application/json'
+                             })
+  closes = response.json().get('chart').get('result')[0].get('indicators').get('quote')[0].get('close')
+  times = response.json().get('chart').get('result')[0].get('timestamp')
+# #print(closes)
+
+  df = pd.DataFrame({'closes': closes, 'times': times})
+  df['times'] = pd.to_datetime(df['times'], unit='s')
+  df = df.set_index('times')
+  df = df.interpolate()
+  t1 = times[0]
+  t2 = times[-1]
+  c1 = closes[0]
+  c2 = closes[-1]
+  print("t1: "+str(t1)+" t2: "+str(t2)+" c1: "+str(c1)+" c2: "+str(c2))
+  df.to_pickle("p_"+str(t1)+"_"+str(t2)+".pick")
+  return t1
+
+seven_days_in_s = 60*60*24*7
+time_ago_in_past = seven_days_in_s * 4
+start_time_in_s = calendar.timegm(time.gmtime())
+
+for t in range(start_time_in_s - time_ago_in_past, start_time_in_s, seven_days_in_s):
+  download_period(t, t+seven_days_in_s)
